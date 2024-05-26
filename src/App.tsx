5444ad352cbf410fc4df5edb9a33a90221b73a05
Khambazarov <khambazarovrenat@gmail.com>
2024-05-26 19:56:40 +0200

    add feat: current location

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ interface WeatherData {
   location: {
     name: string;
     country: string;
+    // localtime: string;
   };
   current: {
     condition: {
@@ -28,11 +29,9 @@ interface WeatherData {
       astro: {
         sunrise: string;
         sunset: string;
-        moonrise: string;
-        moonset: string;
       };
       hour: {
-        time: string;
+        time: [];
       };
     }[];
   };
@@ -45,26 +44,33 @@ function App() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      if (city) {
-        const current = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=yes`;
-        const forecast = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&aqi=yes`;
-        try {
-          const response1 = await fetch(current);
-          const data1 = await response1.json();
-          const response2 = await fetch(forecast);
-          const data2 = await response2.json();
-          setData({ ...data1, ...data2 });
-        } catch (error) {
-          console.error("Error:", error);
-        }
+    const fetchData = async (city: string) => {
+      const current = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
+      const forecast = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1&aqi=no&alerts=no`;
+      try {
+        const response1 = await fetch(current);
+        const data1 = await response1.json();
+        const response2 = await fetch(forecast);
+        const data2 = await response2.json();
+        setData({ ...data1, ...data2 });
+      } catch (error) {
+        console.error("Error:", error);
       }
     };
-    fetchData();
-    // abort signal
+    
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      fetchData(`${latitude},${longitude}`);
+    });
+
+    if (city) {
+      fetchData(city);
+    }
   }, [city]);
 
   const LOCATION = data?.location.name;
+
+  // const LOCAL_TIME = data?.location.localtime
 
   const COUNTRY = data?.location.country;
 
@@ -97,20 +103,27 @@ function App() {
   const HUMIDITY = data && `Humidity: ${data?.current.humidity ?? ""}%`;
 
   const SUNRISE =
-    data && `${data?.forecast.forecastday.map((i) => i.astro.sunrise)} – `;
+    data &&
+    `${
+      data?.forecast.forecastday
+        .map((i) => i.astro.sunrise)
+        .join("")
+        .split(" ")[0]
+    }`;
 
-  const SUNSET = data && data?.forecast.forecastday.map((i) => i.astro.sunset);
+  const sunsetConvertTo24Hours = () => {
+    const timeSplitting =
+      data &&
+      data?.forecast.forecastday
+        .map((i) => i.astro.sunset)
+        .join("")
+        .split(" ")[0];
 
-  const MOONRISE =
-    data && `${data?.forecast.forecastday.map((i) => i.astro.moonrise)} – `;
-
-  const MOONSET =
-    data && data?.forecast.forecastday.map((i) => i.astro.moonset);
-
-  // const HOURS =
-  //   data && data?.forecast.forecastday.map((i) => i.hour).map((h) => h.time);
-
-  // console.log("HOURS:", HOURS);
+    const convertMinuts = timeSplitting?.split(":")[1];
+    const convertHour = Number(timeSplitting?.split(":")[0]) + 12;
+    const joinTime = `${convertHour}:${convertMinuts}`;
+    return joinTime;
+  };
 
   const inputCity = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -122,31 +135,31 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="location">{LOCATION}</h1>
-      <h2 className="country">{COUNTRY}</h2>
-      <div className="condition">
-        {CONDITION}
-        <img src={ICON} alt="" />
-      </div>
-      <div className="temperature">{TEMPERATURE}</div>
-      <div className="perseived">{PERCEIVED_TEMP}</div>
-      <div className="temp-min-max">
-        {TEMP_MIN}
-        {TEMP_MAX}
-      </div>
-      <div className="wind-speed">{WIND_KPH}</div>
-      <div className="wind-dir">{WIND_DIR}</div>
-      <div className="air-humidity">{HUMIDITY}</div>
-      <div className="sunrise-sunset">
-        <img src={SUN_ICON} />
-        {SUNRISE}
-        {SUNSET}
-      </div>
-      <div className="moonrise-moonset">
-        {MOONRISE}
-        {MOONSET}
-      </div>
-      {/* <div className="hours">{HOURS}</div> */}
+      {data && (
+        <>
+          <h1 className="location">{LOCATION}</h1>
+          <h2 className="country">{COUNTRY}</h2>
+          {/* <h2 className="local-time">{LOCAL_TIME}</h2> */}
+          <div className="sunrise-sunset">
+            {SUNRISE}
+            <img src={SUN_ICON} />
+            {data && sunsetConvertTo24Hours()}
+          </div>
+          <div className="condition">
+            {CONDITION}
+            <img src={ICON} alt="" />
+          </div>
+          <div className="temperature">{TEMPERATURE}</div>
+          <div className="perseived">{PERCEIVED_TEMP}</div>
+          <div className="temp-min-max">
+            {TEMP_MIN}
+            {TEMP_MAX}
+          </div>
+          <div className="wind-speed">{WIND_KPH}</div>
+          <div className="wind-dir">{WIND_DIR}</div>
+          <div className="air-humidity">{HUMIDITY}</div>
+        </>
+      )}
       <form className="form" onSubmit={inputCity}>
         <input
           type="text"
