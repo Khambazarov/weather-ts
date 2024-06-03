be28501d15de2487379d480c8eef05e235b087a6
Khambazarov <khambazarovrenat@gmail.com>
2024-06-03 15:40:26 +0200

    add feat: wind-direction

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef, FC } from "react";
 import { FiSunset, FiSunrise } from "react-icons/fi";
 import {
-  FaTemperatureArrowUp,
+  // FaTemperatureArrowUp,
   FaTemperatureHalf,
-  FaTemperatureArrowDown,
+  // FaTemperatureArrowDown,
 } from "react-icons/fa6";
-import { WiHumidity } from "react-icons/wi";
+import { WiHumidity, WiStrongWind, WiDirectionUp } from "react-icons/wi";
 import "./App.css";
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
@@ -86,22 +86,25 @@ const App: FC = () => {
 
   const LOCATION = data?.location?.name;
   const LOCAL_TIME = data?.location.localtime.split(" ")[1];
+  console.log(LOCAL_TIME);
   const COUNTRY = data?.location.country;
   const CONDITION = data?.current.condition.text;
   const ICON = data && `https:${data?.current.condition.icon}`;
   const SUN_ICON =
     data && `https://cdn.weatherapi.com/weather/64x64/day/113.png`;
   const TEMPERATURE = data && `${Math.round(data.current.temp_c)}`;
-  const PERCEIVED_TEMP =
-    data && `Perceived: ${Math.round(data.current.feelslike_c)}`;
-  const TEMP_MIN =
-    data &&
-    `${data?.forecast.forecastday.map((i) => Math.round(i.day.mintemp_c))} – `;
-  const TEMP_MAX =
-    data &&
-    `${data?.forecast.forecastday.map((i) => Math.round(i.day.maxtemp_c))}`;
-  const WIND_KPH = data && `Wind: ${data?.current.wind_kph ?? ""} kph`;
-  const WIND_DIR = data && `Wind Direction: ${data?.current.wind_dir}`;
+  // const PERCEIVED_TEMP =
+  //   data && `Perceived: ${Math.round(data.current.feelslike_c)}`;
+  // const TEMP_MIN =
+  //   data &&
+  //   `${data?.forecast.forecastday.map((i) => Math.round(i.day.mintemp_c))} – `;
+  // const TEMP_MAX =
+  //   data &&
+  //   `${data?.forecast.forecastday.map((i) => Math.round(i.day.maxtemp_c))}`;
+  const WIND_MS =
+    data && `${Math.round(data?.current.wind_kph / 3.6) ?? ""} m/s`;
+  const WIND_DIR = data && `${data?.current.wind_dir}`;
+  console.log(WIND_DIR);
   const HUMIDITY = data && `${data?.current.humidity ?? ""}`;
   const SUNRISE =
     data &&
@@ -141,6 +144,8 @@ const App: FC = () => {
     const DAY = 24 * 60;
     const CIRCLE = Math.PI * 4;
     const timeSplitting = data && data?.location.localtime.split(" ")[1];
+    const time = data ? new Date(data.location.localtime) : new Date();
+    console.log(time);
     const convertedMinutes = Number(timeSplitting?.split(":")[1]);
     const convertedHour = Number(timeSplitting?.split(":")[0]) * 60 + 180; // +180 Minuten = 90deg ?? muss andere lösung finden!
     const currentTimeInMinuts = convertedHour + convertedMinutes;
@@ -161,6 +166,30 @@ const App: FC = () => {
     const getHours = Number(LOCAL_TIME?.split(":")[0]);
     return getHours * 60 + getMinutes;
   };
+
+  const windDirection = () => {
+    if (data && WIND_DIR === "N") return "0deg";
+    if (data && WIND_DIR === "NNE") return "22.5deg";
+    if (data && WIND_DIR === "NE") return "45deg";
+    if (data && WIND_DIR === "ENE") return "67.5deg";
+    if (data && WIND_DIR === "E") return "90deg";
+    if (data && WIND_DIR === "ESE") return "112.5deg";
+    if (data && WIND_DIR === "SE") return "135deg";
+    if (data && WIND_DIR === "SSE") return "157.5deg";
+    if (data && WIND_DIR === "S") return "180deg";
+    if (data && WIND_DIR === "SSW") return "202.5deg";
+    if (data && WIND_DIR === "SW") return "225deg";
+    if (data && WIND_DIR === "WSW") return "247.5deg";
+    if (data && WIND_DIR === "W") return "270deg";
+    if (data && WIND_DIR === "WNW") return "292.5deg";
+    if (data && WIND_DIR === "NW") return "315deg";
+    if (data && WIND_DIR === "NNW") return "337.5deg";
+  };
+
+  const windRotation =
+    {
+      N: "0deg",
+    }?.[WIND_DIR ?? ""] ?? "n.a.";
 
   return (
     <div className="container">
@@ -187,10 +216,35 @@ const App: FC = () => {
                   <span>{SUNRISE}</span>
                 </div>
                 <div className="local-temp">
-                  <span className="temp">{TEMPERATURE}</span>
-                  <span>
-                    <FaTemperatureHalf />
-                  </span>
+                  <div className="temp-icons">
+                    <div className="icon-thermometer">
+                      <FaTemperatureHalf />
+                    </div>
+                    <div className="temps">{TEMPERATURE}</div>
+                  </div>
+                  <div className="wind-speed-dir">
+                    <div
+                      className="wind-direction-icon"
+                      style={{
+                        rotate: `${windDirection()}`,
+                      }}
+                    >
+                      <span className="arrow-icon">
+                        <WiDirectionUp />
+                      </span>
+                    </div>
+                    <span>
+                      <WiStrongWind />
+                    </span>
+                    <div className="wind-speed">{WIND_MS}</div>
+                    <div className="wind-dir">{WIND_DIR}</div>
+                  </div>
+                  <div className="air-humidity">
+                    <span>{HUMIDITY}</span>
+                    <span className="humidity-icon">
+                      <WiHumidity />
+                    </span>
+                  </div>
                 </div>
                 <div className="sun-icons-wrapper">
                   <FiSunset />
@@ -217,10 +271,25 @@ const App: FC = () => {
                   <span>{SUNRISE}</span>
                 </div>
                 <div className="local-temp">
-                  <span className="temp">{TEMPERATURE}</span>
-                  <span>
-                    <FaTemperatureHalf />
-                  </span>
+                  <div className="temp-icons">
+                    <div className="icon-thermometer">
+                      <FaTemperatureHalf />
+                    </div>
+                    <div className="temps">{TEMPERATURE}</div>
+                  </div>
+                  <div className="wind-speed-dir">
+                    <span>
+                      <WiStrongWind />
+                    </span>
+                    <div className="wind-speed">{WIND_MS}</div>
+                    <div className="wind-dir">{WIND_DIR}</div>
+                  </div>
+                  <div className="air-humidity">
+                    <span>{HUMIDITY}</span>
+                    <span className="humidity-icon">
+                      <WiHumidity />
+                    </span>
+                  </div>
                 </div>
                 <div className="sun-icons-wrapper">
                   <FiSunset />
@@ -233,13 +302,13 @@ const App: FC = () => {
             {CONDITION}
             <img src={ICON} alt="" />
           </div>
-          <div className="perceived">
+          {/* <div className="perceived">
             {PERCEIVED_TEMP}
             <span>
               <FaTemperatureHalf />
             </span>
-          </div>
-          <div className="temp-min-max">
+          </div> */}
+          {/* <div className="temp-min-max">
             <span>
               <FaTemperatureArrowDown />
             </span>
@@ -248,18 +317,10 @@ const App: FC = () => {
             <span>
               <FaTemperatureArrowUp />
             </span>
-          </div>
-          <div className="wind-speed">{WIND_KPH}</div>
-          <div className="wind-dir">{WIND_DIR}</div>
-          <div className="air-humidity">
-            <span>{HUMIDITY}</span>
-            <span>
-              <WiHumidity />
-            </span>
-          </div>
+          </div> */}
         </>
       ) : (
-        <h2>NOT FOUND, TRY ANOTHER LOCATION...</h2>
+        <h2>LOADING...</h2>
       )}
       <form className="form" onSubmit={inputCity}>
         <input
